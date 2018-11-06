14f43263d2634affcc7e31ceb70967fe0052c696
Eoin Wickens <eoinwickens@gmail.com>
2018-11-07 00:14:25 +0100

    Fully working prototype, send receive with average of temperatures

--- example-unicast.c
+++ example-unicast.c
@@ -1,45 +1,8 @@
-/*
- * Copyright (c) 2007, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         Best-effort single-hop unicast example
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
 
 #include "contiki.h"
 #include "net/rime/rime.h"
 #include <stdio.h>
+#include "dev/sht11/sht11-sensor.h"
 
 /*---------------------------------------------------------------------------*/
 PROCESS(example_unicast_process, "Example unicast");
@@ -67,24 +30,25 @@ static const struct unicast_callbacks unicast_callbacks = {recv_uc, sent_uc};
 static struct unicast_conn uc;
 /*---------------------------------------------------------------------------*/
 
-
+static int tempVal;
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
   printf("broadcast message received from %d.%d: '%s'\n",
          from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
 
-	// REPLY FUNCTION VIA UNICAST
+    // REPLY FUNCTION VIA UNICAST
 	
     linkaddr_t addr;
-
-    packetbuf_copyfrom("Hello", 5); //Might want to change this back to five?
+	char str[5];
+	sprintf(str, "%d", tempVal);
+    packetbuf_copyfrom(str, 5); //Size of the temp val??
     addr.u8[0] = from->u8[0];
     addr.u8[1] = from->u8[1];
     if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
-      unicast_send(&uc, &addr);
+    //printf("Temperature is %d\n", tempVal);
+    unicast_send(&uc, &addr);
     }
-   // unicast_send(&uc,&addr);
 
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
@@ -105,19 +69,18 @@ PROCESS_THREAD(example_unicast_process, ev, data)
 
   while(1) {
     static struct etimer et;
-    linkaddr_t addr;
+    SENSORS_ACTIVATE(sht11_sensor);
     
     etimer_set(&et, CLOCK_SECOND);
     
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+	
+    int val = sht11_sensor.value(SHT11_SENSOR_TEMP);
+    tempVal = (-39.60 + 0.01*val);
+   
 
-    packetbuf_copyfrom("Hello", 5);
-    addr.u8[0] = 1;
-    addr.u8[1] = 0;
-    //if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
-    // unicast_send(&uc, &addr);
-    //}
-
+    etimer_reset(&et);
+    SENSORS_DEACTIVATE(sht11_sensor);
   }
 
   PROCESS_END();
